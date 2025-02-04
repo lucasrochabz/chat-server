f72ec3ac0f5d03f69d048609f09361a617cee25d
lucasrochabz <lucasbezerrar@gmail.com>
2025-02-04 02:33:53 -0300

    add estilo ao chat

--- src/public/chat.js
+++ src/public/chat.js
@@ -1,6 +1,27 @@
 const socket = io('http://localhost:3000');
 
-document.querySelector('button').addEventListener('click', () => {
-  const msg = document.getElementById('message').value;
-  socket.emit('chat message', msg); // Envia apenas para o cliente que enviou a mensagem
-});
+const inputElement = document.querySelector('#message');
+const outputElement = document.querySelector('#output');
+const btnSend = document.querySelector('#btn-send');
+
+function handleMessage() {
+  let message = inputElement.value;
+
+  if (message === '') return null;
+
+  const pElement = document.createElement('p');
+  pElement.textContent = message;
+  outputElement.appendChild(pElement);
+  socket.emit('chat message', message); // Envia apenas para o cliente que enviou a mensagem
+
+  inputElement.value = '';
+}
+
+function handleEvent(event) {
+  if (event.key === 'Enter' || event.type === 'click') {
+    handleMessage();
+  }
+}
+
+inputElement.addEventListener('keydown', handleEvent);
+btnSend.addEventListener('click', handleEvent);
