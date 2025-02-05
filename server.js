6afc7e7590f559a84b398840560f3ebb8fede361
lucasrochabz <lucasbezerrar@gmail.com>
2025-02-05 18:57:12 -0300

    organizando estrutura do projeto

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const { server } = require('./src/app');
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const socketIo = require('socket.io');
+const chatRouter = require('./src/routes/chatRoute');
+const chatSocket = require('./src/sockets/chatSocket');
 
+// Instância do Express
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/chat', chatRouter);
+
+// Criando o servidor HTTP
+const server = http.createServer(app);
+
+// Configurando o Socket.io
+const io = socketIo(server);
+chatSocket.setupChatSocket(io);
+
+// Iniciando o servidor
 server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server running on port 3000');
 });
--- src/controllers/chatsController.js
+++ src/controllers/chatsController.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 // Serve o arquivo HTML para o chat
 const getChatPage = (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
 };
 
 module.exports = { getChatPage };
