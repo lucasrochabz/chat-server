3f65c832c36c61e90dc783d0f3d74f47de7bead4
lucasrochabz <lucasbezerrar@gmail.com>
2025-02-07 15:01:11 -0300

    add ip ao usuário

--- public/chat.js
+++ public/chat.js
@@ -4,6 +4,24 @@ const inputElement = document.querySelector('#message');
 const outputElement = document.querySelector('#output');
 const btnSend = document.querySelector('#btn-send');
 
+let mySocketId = null;
+
+function renderMessage(message) {
+  const pElement = document.createElement('p');
+  if (message.id === mySocketId) {
+    pElement.classList.add('my-message');
+    pElement.textContent = `Você: ${message.text}`;
+  } else if (message.id === 'server') {
+    pElement.classList.add('server');
+    pElement.textContent = `Servidor: ${message.text}`;
+  } else {
+    pElement.classList.add('other-message');
+    pElement.textContent = `Usuário: ${message.text}`;
+  }
+
+  outputElement.appendChild(pElement);
+}
+
 function handleMessage() {
   const message = inputElement.value.trim(); // Remove espaços extras
 
@@ -23,18 +41,19 @@ function handleEvent(event) {
 inputElement.addEventListener('keydown', handleEvent);
 btnSend.addEventListener('click', handleEvent);
 
+socket.on('connect', () => {
+  mySocketId = socket.id;
+});
+
 // Escuta o histórico de mensagens enviadas pelo servidor
 socket.on('chat history', (messages) => {
-  messages.forEach((message) => {
-    const pElement = document.createElement('p');
-    pElement.textContent = message;
-    outputElement.appendChild(pElement);
-  });
+  messages?.forEach(renderMessage);
+  // const pElement = document.createElement('p');
+  // pElement.textContent = message;
+  // outputElement.appendChild(pElement);
 });
 
 // Escuta mensagens do servidor e exibe no chat
 socket.on('chat message', (message) => {
-  const pElement = document.createElement('p');
-  pElement.textContent = message;
-  outputElement.appendChild(pElement);
+  renderMessage(message);
 });
--- src/sockets/chatSocket.js
+++ src/sockets/chatSocket.js
@@ -3,9 +3,11 @@ const chatService = require('../services/chatService');
 const setupChatSocket = (io) => {
   // Estabelece a conexão do socket
   io.on('connection', (socket) => {
+    // Obtém o ID do usuário
+    console.log('ID do usuário: ', socket.id);
     // Obtém o IP do usuário
-    const clienIp = socket.handshake.address;
-    console.log(`Um usuário se conectado. IP:${clienIp}`);
+    const clientIp = socket.handshake.address;
+    console.log(`Um usuário se conectado. IP:${clientIp}`);
 
     // Enviar histórico de mensagens para o novo usuário
     const messages = chatService.getHistoryMessages();
@@ -13,9 +15,10 @@ const setupChatSocket = (io) => {
 
     // Envia uma mensagem para todos os clientes conectados
     socket.on('chat message', async (msg) => {
+      const msgData = { id: socket.id, text: msg };
       try {
-        chatService.saveMessage(msg);
-        io.emit('chat message', msg); // Envia a mensagem para todos os clientes
+        chatService.saveMessage(msgData);
+        io.emit('chat message', msgData); // Envia a mensagem para todos os clientes
       } catch (error) {
         console.error('Erro ao salvar a mensagem:', error);
       }
