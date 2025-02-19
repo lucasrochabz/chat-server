7aa874346d92b01e888dfe1a2e4e67e0b8421550
lucasrochabz <lucasbezerrar@gmail.com>
2025-02-18 21:03:29 -0300

    corrigindo mensagem ao salvar

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -2,7 +2,9 @@ const messages = []; // Array para armazenar as mensagens temporariamente
 
 const saveMessage = (msgData) => {
   messages.push(msgData); // Armazena a mensagem na memória
-  console.log(`Salvando mensagem: ${msgData.text}`);
+  console.log(
+    `Salvando mensagem. ID: ${msgData.id}, conteúdo: ${msgData.text}`,
+  );
 };
 
 module.exports = { saveMessage };
